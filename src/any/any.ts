eeb48e4625e6c14a5d6aba3ec03122091251060c
Luiz Pires <lutidev@gmail.com>
2020-08-04 21:41:13 -0300

    anny

---
+++ src/any/any.ts
@@ -0,0 +1,8 @@
+// Só utilizar any em último caso
+function showMessage(msg: any) {
+  return msg
+}
+
+console.log(showMessage([1, 2, 3]))
+console.log(showMessage('Hello'))
+console.log(showMessage(1))
